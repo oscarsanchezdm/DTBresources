cd132b2d180fc5a3198244589b4f2900de340e09
oscarsanchezdm <oscarsanchezdm@gmail.com>
2021-03-04 14:32:11 +0100

    new hashlist

--- makeDB.py
+++ makeDB.py
@@ -16,30 +16,8 @@ hashlist = pd.read_csv(hashlist_path,header=None)
 resource_list = [f for f in listdir(resources_path) if not f.startswith('.') and isfile(join(resources_path, f))]
 resource_list.sort()
 
-#add a column to the hashlist
-newDB = hashlist
-newDB[1] = 0
-
-#search for matches
-r = 0
-for i in range(0,len(hashlist)):
-    if len(resource_list) <= r:
-        break
-
-    act_hash = hashlist.at[i,0]
-    act_resource = resource_list[r]
-
-    while (act_resource < act_hash):
-        r = r+1
-        if len(resource_list) <= r:
-            stop = True
-            break
-        act_resource = resource_list[r]
-
-    if (act_resource == act_hash):
-        newDB.at[i,1] = 1
-
 #write DB to a file
+newDB = hashlist
 newDB.to_csv(newdb_path, index=False, header=False)
 
 #generate DB file hash and write it to a file
